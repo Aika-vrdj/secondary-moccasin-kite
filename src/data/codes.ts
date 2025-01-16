41ddb7819a15cd839d4f00751d84a9c3444690cd
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-01-16 11:30:04 -0500

    Update codes.ts
    added redeem codes pt1

--- src/data/codes.ts
+++ src/data/codes.ts
@@ -7,7 +7,7 @@ export const redeemCodes: RedeemCode[] = [
     used: false
   },
   {
-    code: 'CYBERPUNK',
+    code: 'BIRTHDAY2025',
     rp: 200,
     used: false
   },
@@ -27,8 +27,414 @@ export const redeemCodes: RedeemCode[] = [
     used: false
   },
   {
+    code: 'TESTER',
+    rp: 1000,
+    used: false
+  },
+  {
     code: 'DIGITALDREAM',
-    rp: 500,
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'DRONES25',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'REBEL14',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'FALLOUT87',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'INSURGE42',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'OUTLAW31',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SHADOW19',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'UPRISING76',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'IRONFIST03',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'LIBERTY99',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'RESIST17',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'HAVOC22',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'OVERTHROW88',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SABOTEUR55',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'ANARCHY09',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'DOMINION64',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'BREAKOUT33',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'CYBERREX11',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'REDDAWN82',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'FREEZE01',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'RADIANT56',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'EXODUS42',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'WARZONE63',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'NEMESIS08',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SUBVERT77',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'BLACKSUN29',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'VANGUARD85',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'BARRICADE91',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'NIGHTFALL37',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'STALKER66',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'REQUIEM04',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'CRIMSON89',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'DYSTOPE23',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'VEIL77',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'REBIRTH30',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'ECLIPSE57',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'DISRUPT20',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SPARROW12',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'STRIKER44',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'PHOENIX73',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'CONFLICT59',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SHADOW84',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'HAVOC28',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'FALLEN93',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'ASUNDER15',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'MARAUDER67',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'OBSIDIAN46',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'RESIST99',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'SILENCER18',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'ABYSS40',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'NOVA62',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'CIPHER31',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'REVENANT71',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'VORTEX52',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'IRONCLAD96',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'ECHO24',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'DRIFTER82',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'WASTELAND05',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'FORSAKEN38',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'UPRISING43',
+    rp: 100,
+    used: false
+  },
+  {
+    code: 'VIGILANT35',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '47RAVAGER',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '82VOID',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '56RUIN',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '30SHADOW',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '99HAVOC',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '14FALLEN',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '21REBORN',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '63EMBER',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '80DEFIANT',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '48CRUCIBLE',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '72DOOM',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '05WRAITH',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '39LEGION',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '26REAPER',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '88RIOT',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '33VORTEX',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '09BARRICADE',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '50RECKON',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '42EXPERT',
+    rp: 100,
+    used: false
+  },
+  {
+    code: '66WARHEAD',
+    rp: 100,
     used: false
   }
 ];
+  
